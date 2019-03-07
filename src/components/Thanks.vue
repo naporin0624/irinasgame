7f055a4c76379fb72df6b9c99a1607110c0f3778
naporitan <naporin0624@gmail.com>
2019-03-07 21:17:49 +0900

    ごめん”

--- src/components/Thanks.vue
+++ src/components/Thanks.vue
@@ -2,11 +2,12 @@
   div.wrapper
     h1 誕生日おめでとうございます.
     h1 名取さんすきです
-    a(href="https://twitter.com/zuho_cpp")
-      p イラストを貸してくださった方: 寝落ち🍆さん
-    a(href="https://twitter.com/naporin24690")
-      p 作成者
     a(href="https://twitter.com/sana_natori")
       p 偉大なる名取さな様
+    //- a(href="https://twitter.com/zuho_cpp")
+    //-   p イラストを貸してくださった方: 寝落ち🍆さん
+    a(href="https://twitter.com/naporin24690")
+      p 作成者
+
 
 </template>
